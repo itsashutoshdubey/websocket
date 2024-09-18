daa35eaa6339f3d6f0acda7cb7f81582952e157d
itsas <itsashutoshdubey@gmail.com>
2024-09-18 21:58:02 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const ws = require('ws')
+const server = new ws.Server({port: '3000'})
+
+server.on('connection', socket => {
+    socket.on('message', message => {
+        
+        socket.send('${message}')
+    })
+})
